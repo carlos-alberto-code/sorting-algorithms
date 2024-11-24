294951caf766799d2b0305e8af9222587e447637
Carlos Alberto Baltazar Hinojosa <A840159943@my.uvm.edu.mx>
2024-11-24 16:41:03 -0600

    Se creo la versión con clases y con excepciones

--- main.py
+++ main.py
@@ -1,26 +1,30 @@
 import time
 from util.logger import log_results
 from util.numbers_generator import generate_numbers
-from sorting_algorithms.algorithms import bubble_sort, selection_sort, insertion_sort, merge_sort
+from sorting_algorithms.algorithms import * # Importamos todas las clases de algoritmos
 
 with open("log.txt", "w") as log_file:
     log_file.write("")
 
-cases = ["best", "average", "worst"]
+cases = ["average", "worst"]
 elements = [1_000]
-algorithms = {
-    "Bubble Sort": bubble_sort,
-    "Selection Sort": selection_sort,
-    "Insertion Sort": insertion_sort,
-    "Merge Sort": merge_sort
-}
+algorithms = [
+    BubbleSort(), SelectionSort(),
+    InsertionSort(), MergeSort()
+]
 
 for case in cases:
     for element in elements:
-        numbers = generate_numbers(case, element)
-        for i, (name, func) in enumerate(algorithms.items()):
+        try:
+            numbers = generate_numbers(case, element)
+        except ValueError as e:
+            print(f'Error al generar los números: {e}')
+            continue
+        for i, algorithm in enumerate(algorithms):
             start = time.time()
-            func(numbers.copy())
+            algorithm.sort(numbers.copy())
             end = time.time()
             is_last_algorithm = i == len(algorithms) - 1
-            log_results(case, element, name, end - start, is_last_algorithm)
+            log_results(
+                case, element, algorithm.__class__.__name__, end - start, is_last_algorithm
+            )
--- sorting_algorithms/algorithms.py
+++ sorting_algorithms/algorithms.py
@@ -1,71 +1,66 @@
 from typing import List
+from abc import ABC, abstractmethod
 
-def bubble_sort(numbers: List[int]):
-    """ Algoritmo de oredenación burbuja:
-    Recorre la lista de números y compara cada elemento con el siguiente.
-    Si el elemento actual es mayor que el siguiente, los intercambia.
-    Repite el proceso hasta que no se realice ningún intercambio.
-    """
-    n = len(numbers)
-    for i in range(n):
-        for j in range(0, n-i-1):
-            if numbers[j] > numbers[j+1]:
-                numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
-    return numbers
+class SortingAlgorithm(ABC):
+    # Esta es una clase abstracta que define un método abstracto llamado sort
+    @abstractmethod
+    def sort(self, numbers: List[int]) -> List[int]:
+        # Este método debe ser implementado por las clases que hereden de SortingAlgorithm
+        # Con este método se busca mejorar la evolución del código
+        # Así se puede depender de la clase abstracta SortingAlgorithm en lugar de depender de las clases concretas o de funciones como en el caso de la versión 1.
+        pass
 
-def selection_sort(numbers: List[int]):
-    """ Algoritmo de ordenación por selección:
-    Recorre la lista de números y busca el menor elemento.
-    Intercambia el menor elemento con el primer elemento de la lista.
-    Repite el proceso con el resto de la lista.
-    """
-    n = len(numbers)
-    for i in range(n):
-        min_index = i
-        for j in range(i+1, n):
-            if numbers[j] < numbers[min_index]:
-                min_index = j
-        numbers[i], numbers[min_index] = numbers[min_index], numbers[i]
-    return numbers
-
-def insertion_sort(numbers: List[int]):
-    """ Algoritmo de ordenación por inserción:
-    Recorre la lista de números y compara cada elemento con los anteriores.
-    Si el elemento actual es menor que el anterior, los intercambia.
-    Repite el proceso hasta que el elemento actual sea mayor que el anterior.
-    """
-    n = len(numbers)
-    for i in range(1, n):
-        key = numbers[i]
-        j = i - 1
-        while j >= 0 and key < numbers[j]:
-            numbers[j + 1] = numbers[j]
-            j -= 1
-        numbers[j + 1] = key
-    return numbers
-
-def merge_sort(numbers: List[int]):
-    """ Algoritmo de ordenación por mezcla:
-    Divide la lista en mitades hasta que cada sublista tenga un solo elemento.
-    Luego combina las sublistas en orden ascendente.
-    """
-    def merge(left: List[int], right: List[int]) -> List[int]:
-        result = []
-        i = j = 0
-        while i < len(left) and j < len(right):
-            if left[i] < right[j]:
-                result.append(left[i])
-                i += 1
-            else:
-                result.append(right[j])
-                j += 1
-        result.extend(left[i:])
-        result.extend(right[j:])
-        return result
-
-    if len(numbers) <= 1:
+class BubbleSort(SortingAlgorithm):
+    def sort(self, numbers: List[int]) -> List[int]:
+        n = len(numbers)
+        for i in range(n):
+            for j in range(0, n-i-1):
+                if numbers[j] > numbers[j+1]:
+                    numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
         return numbers
-    middle = len(numbers) // 2
-    left = merge_sort(numbers[:middle])
-    right = merge_sort(numbers[middle:])
-    return merge(left, right)
+
+class SelectionSort(SortingAlgorithm):
+    def sort(self, numbers: List[int]) -> List[int]:
+        n = len(numbers)
+        for i in range(n):
+            min_index = i
+            for j in range(i+1, n):
+                if numbers[j] < numbers[min_index]:
+                    min_index = j
+            numbers[i], numbers[min_index] = numbers[min_index], numbers[i]
+        return numbers
+
+class InsertionSort(SortingAlgorithm):
+    def sort(self, numbers: List[int]) -> List[int]:
+        n = len(numbers)
+        for i in range(1, n):
+            key = numbers[i]
+            j = i - 1
+            while j >= 0 and key < numbers[j]:
+                numbers[j + 1] = numbers[j]
+                j -= 1
+            numbers[j + 1] = key
+        return numbers
+
+class MergeSort(SortingAlgorithm):
+    def sort(self, numbers: List[int]) -> List[int]:
+        def merge(left: List[int], right: List[int]) -> List[int]:
+            result = []
+            i = j = 0
+            while i < len(left) and j < len(right):
+                if left[i] < right[j]:
+                    result.append(left[i])
+                    i += 1
+                else:
+                    result.append(right[j])
+                    j += 1
+            result.extend(left[i:])
+            result.extend(right[j:])
+            return result
+
+        if len(numbers) <= 1:
+            return numbers
+        middle = len(numbers) // 2
+        left = self.sort(numbers[:middle])
+        right = self.sort(numbers[middle:])
+        return merge(left, right)
